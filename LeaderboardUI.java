c79937a2e9823a927bb128887d224c55b996c879
Philip Waldron <philip_waldron@yahoo.com>
2016-11-01 01:23:35 +0000

    Redone LeaderboardUI

--- LeaderboardUI.java
+++ LeaderboardUI.java
@@ -1,4 +1,183 @@
-public class ViewLeaderboard extends JFrame {
-	//will create new window using Jframe, populated with a list of the players and their stats from the leaderboard object.
-	//The user will be able to sort the leaderboard and apply a filter
+// Created using Netbeans 8.2
+
+/**
+ *
+ * @author Philip Waldron
+ */
+public class LeaderboardUI extends javax.swing.JFrame {
+
+    /**
+     * Creates new form LeaderboardUI
+     */
+    public LeaderboardUI() {
+        initComponents();
+    }
+                      
+    private void initComponents() {
+
+        currentStandings = new javax.swing.JLabel();
+        sortMethod = new javax.swing.JLabel();
+        alphabeticSortButton = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        leaderboardTable = new javax.swing.JTable();
+        winSortButton = new javax.swing.JButton();
+        wLRatioSortButton = new javax.swing.JButton();
+        filter = new javax.swing.JLabel();
+        filterTextArea = new javax.swing.JTextField();
+        filterButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        currentStandings.setText("Current standings");
+
+        sortMethod.setText("Sort Method");
+
+        alphabeticSortButton.setText("Sort Alphabetically");
+        alphabeticSortButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                alphabeticSortButtonActionPerformed(evt);
+            }
+        });
+
+        leaderboardTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Name", "Wins", "Losses", "W/L Ratio"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Double.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(leaderboardTable);
+        if (leaderboardTable.getColumnModel().getColumnCount() > 0) {
+            leaderboardTable.getColumnModel().getColumn(0).setResizable(false);
+            leaderboardTable.getColumnModel().getColumn(1).setResizable(false);
+        }
+
+        winSortButton.setText("Sort By Wins");
+
+        wLRatioSortButton.setText("Sort by W/L Ratio");
+
+        filter.setText("Filter");
+
+        filterTextArea.setText("Enter name");
+
+        filterButton.setText("Apply Filter");
+        filterButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                filterButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(currentStandings))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(filter)
+                    .addComponent(sortMethod)
+                    .addComponent(wLRatioSortButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(winSortButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(alphabeticSortButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(filterTextArea)
+                    .addComponent(filterButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(currentStandings, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(sortMethod, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(alphabeticSortButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(winSortButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(wLRatioSortButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(filter)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(filterTextArea, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(filterButton))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }                    
+
+    private void alphabeticSortButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                     
+        // TODO add your handling code here:
+    }                                                    
+
+    private void filterButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        // TODO add your handling code here:
+    }                                            
+
+    public static void main(String args[]) {
+        /* (OPTIONAL) Set the Nimbus look and feel.
+		   If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LeaderboardUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LeaderboardUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LeaderboardUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LeaderboardUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+		*/
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LeaderboardUI().setVisible(true);
+            }
+        });
+    }
+	
+    private javax.swing.JButton alphabeticSortButton;
+    private javax.swing.JLabel currentStandings;
+    private javax.swing.JLabel filter;
+    private javax.swing.JButton filterButton;
+    private javax.swing.JTextField filterTextArea;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable leaderboardTable;
+    private javax.swing.JLabel sortMethod;
+    private javax.swing.JButton wLRatioSortButton;
+    private javax.swing.JButton winSortButton;                
 }
